2885181810a497c1a58f8fcbd687b901afc134df
Isaac Blanco <63171746+isaacblanco@users.noreply.github.com>
2024-10-28 09:53:10 +0100

    Re-starting
    Moviendo el proyecto a un github público

---
+++ README.md
@@ -0,0 +1,13 @@
+# TFM - TO DO Focus
+
+By Isaac Blanco Peco
+
+## Instalación
+
+```
+npm i
+cd ./batuta-culinaria
+ionic-serve
+```
+
+ionic-serve lanzará la app en el navegador en http://localhost:8100
---
+++ to-do-focus/src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  IonApp,
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+  IonRouterLink,
+  IonRouterOutlet,
+  IonSplitPane,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  archiveOutline,
+  archiveSharp,
+  bookmarkOutline,
+  bookmarkSharp,
+  heartOutline,
+  heartSharp,
+  mailOutline,
+  mailSharp,
+  paperPlaneOutline,
+  paperPlaneSharp,
+  trashOutline,
+  trashSharp,
+  warningOutline,
+  warningSharp,
+} from 'ionicons/icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonNote,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterLink,
+    IonRouterOutlet,
+  ],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Today', url: '/folder/Today', icon: 'warning' },
+    { title: 'Project', url: '/folder/Project', icon: 'warning' },
+    { title: 'Settings', url: '/folder/Settings', icon: 'warning' },
+  ];
+  public labels = []; // 'Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'
+  constructor() {
+    addIcons({
+      mailOutline,
+      mailSharp,
+      paperPlaneOutline,
+      paperPlaneSharp,
+      heartOutline,
+      heartSharp,
+      archiveOutline,
+      archiveSharp,
+      trashOutline,
+      trashSharp,
+      warningOutline,
+      warningSharp,
+      bookmarkOutline,
+      bookmarkSharp,
+    });
+  }
+}
